06d6605fc001d2ebffa7675f4c6dcb87341a12fd
Neko_chan <mushbeen25@gmail.com>
2024-07-01 16:44:21 +0900

    Lagless,2char

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <cstdint>
 
 
+
 CAN can1(PA_11, PA_12, (int)1e6);
 BufferedSerial serial(USBTX, USBRX, 115200);
 int16_t output[4] = {0, 0, 0, 0};
@@ -17,36 +18,51 @@ int main()
 {
 
 while (1) {
-    char data;
-    int deg;
-    int ledst;
+    char buff;
+    char data[3];
+    //int deg;
+    int CAN_Send;
 
     if (serial.readable()){
-    serial.read(&data, sizeof(data));
-    if(data=='5'){
+        int i=0;
+        while(1){
+            serial.read(&buff, sizeof(buff));
+            data[i]=buff;
+            serial.write("10", sizeof("10"));
+            i++;
+            if (buff == '\0'){
+                break;
+            }
+        }
+    //serial.write("wi",sizeof("wi"));
+    if(strcmp(data, "10\0") == 0){
         output[0] = 4000;
-    } else if(data=='4'){
+    } else if(strcmp(data, "11\0") == 0){
         output[0] = -2000;
-    }else if (data == '7') {
-        ledst=1;
+    }else if (strcmp(data, "w\0") == 0) {
+        CAN_Send=1;
     
-    }else if(data=='8'){
-        ledst=0;
+    }else if(strcmp(data, "s\0") == 0){
+        CAN_Send=-1;
 
+    }
+    else if(strcmp(data, "0\0") == 0){
+        CAN_Send=0;
+    
     }
     else{
         output[0] = 0;
     }
-    if (ledst==1){
+    }
+if (CAN_Send==1){
         output[0]=4000;
+    }else if(CAN_Send==-1){
+        output[0]=-4000;
     }else{
         output[0]=0;
     }
-    CANMessage msg(1, (const uint8_t *)output, 8);
+    CANMessage msg(4, (const uint8_t *)output, 8);
     can1.write(msg);
-
-}
-
 }
 
 }
