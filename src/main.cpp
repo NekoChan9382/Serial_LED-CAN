e3d401b9ee7f589bb0c47b19e79cc1c4e80d0307
Neko_chan <mushbeen25@gmail.com>
2024-07-03 16:04:03 +0900

    Multi_press(Issue)

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,6 @@
-//main.cpp
-#include "drivers/include/drivers/BufferedSerial.h" 
-#include "drivers/include/drivers/DigitalOut.h"
-#include "drivers/include/drivers/interfaces/InterfaceCAN.h"
 #include "mbed.h"
 #include "Servo.hpp"
 #include <cstdint>
-
-
 
 CAN can1(PA_11, PA_12, (int)1e6);
 BufferedSerial serial(USBTX, USBRX, 115200);
@@ -17,52 +11,80 @@ DigitalOut led(LED1);
 int main()
 {
 
-while (1) {
-    char buff;
-    char data[3];
-    //int deg;
-    int CAN_Send;
+    while (1)
+    {
+        char buff;
+        char data[5];
+        // int deg;
+        int CAN_ws = 0;
+        int CAN_ad = 0;
 
-    if (serial.readable()){
-        int i=0;
-        while(1){
-            serial.read(&buff, sizeof(buff));
-            data[i]=buff;
-            serial.write("10", sizeof("10"));
-            i++;
-            if (buff == '\0'){
-                break;
+        if (serial.readable())
+        {
+            int i = 0;
+            while (1)
+            {
+                serial.read(&buff, sizeof(buff));
+                data[i] = buff;
+                i++;
+                if (buff == '\0')
+                {
+                    break;
+                }
+                serial.write(&data, sizeof(data));
+            }
+            // serial.write("wi",sizeof("wi"));
+            if (strcmp(data, "10\0") == 0)
+            {
+                output[0] = 4000;
+            }
+            else if (strcmp(data, "11\0") == 0)
+            {
+                output[0] = -2000;
+            }
+            else if (strcmp(data, "w\0") == 0)
+            {
+                CAN_ws = 1;
+            }
+            else if (strcmp(data, "s\0") == 0)
+            {
+                CAN_ws = -1;
+            }
+            else if (strcmp(data, "w0\0") == 0 or strcmp(data, "s0\0") == 0)
+            {
+                CAN_ws = 0;
+            }else if (strcmp(data, "a\0") == 0)
+            {
+                CAN_ad = 1;
+            }
+            else if (strcmp(data, "d\0") == 0)
+            {
+                CAN_ad = -1;
+            }
+            else if (strcmp(data, "a0\0") == 0 or strcmp(data, "d0\0") == 0)
+            {
+                CAN_ad = 0;
+            }
+            else
+            {
+                output[0] = 0;
             }
         }
-    //serial.write("wi",sizeof("wi"));
-    if(strcmp(data, "10\0") == 0){
-        output[0] = 4000;
-    } else if(strcmp(data, "11\0") == 0){
-        output[0] = -2000;
-    }else if (strcmp(data, "w\0") == 0) {
-        CAN_Send=1;
-    
-    }else if(strcmp(data, "s\0") == 0){
-        CAN_Send=-1;
-
+        if (CAN_ws == 1)
+        {
+            output[0] = 4000;
+            led = 1;
+        }
+        else if (CAN_ws == -1)
+        {
+            output[0] = -4000;
+        }
+        else
+        {
+            output[0] = 0;
+            led = 0;
+        }
+        CANMessage msg(4, (const uint8_t *)output, 8);
+        can1.write(msg);
     }
-    else if(strcmp(data, "0\0") == 0){
-        CAN_Send=0;
-    
-    }
-    else{
-        output[0] = 0;
-    }
-    }
-if (CAN_Send==1){
-        output[0]=4000;
-    }else if(CAN_Send==-1){
-        output[0]=-4000;
-    }else{
-        output[0]=0;
-    }
-    CANMessage msg(4, (const uint8_t *)output, 8);
-    can1.write(msg);
-}
-
 }
