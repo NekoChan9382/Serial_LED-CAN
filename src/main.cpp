f5922473621d86f810b4e5a8dd81cfb57ee4be40
Neko_chan <mushbeen25@gmail.com>
2024-07-08 15:55:12 +0900

    Comment out

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,11 @@
 #include "Servo.hpp"
 #include <cstdint>
 
-CAN can1(PA_11, PA_12, (int)1e6);
-BufferedSerial serial(USBTX, USBRX, 115200);
-int16_t output[4] = {0, 0, 0, 0};
-CANMessage msg;
-DigitalOut led(LED1);
+CAN can1(PA_11, PA_12, (int)1e6); //CAN初期化
+BufferedSerial serial(USBTX, USBRX, 115200); //シリアル初期化
+int16_t output[4] = {0, 0, 0, 0}; //CAN送信データ
+CANMessage msg; //CANメッセージ定義
+DigitalOut led(LED1); //LED初期化
 
 int main()
 {
@@ -15,32 +15,33 @@ int main()
     {
         
         // int deg;
-        int8_t CAN_Send;
+        int8_t CAN_Send; //CAN送信データ保存
 
 
         if (serial.readable())
         {
-            int i = 0;
-            char buff='0';
-            char data[5];
+            int i = 0; //繰り返し変数
+            char buff='0'; //シリアル受信
+            char data[5]; //受信データ保存
 
             while ('buff' != '\0' and i < 5)
             {
-                serial.read(&buff, sizeof(buff));
-                data[i] = buff;
+                serial.read(&buff, sizeof(buff)); //シリアル受信
+                data[i] = buff; //受信データ保存
                 i++;
 
             }
 
-            if (strcmp(data, "10\0") == 0)
+            //データ処理
+            /*if (strcmp(data, "10\0") == 0)
             {
                 output[0] = 8000;
             }
             else if (strcmp(data, "11\0") == 0)
             {
                 output[0] = -8000;
-            }
-            else if (strcmp(data, "w\0") == 0)
+            }*/
+            if (strcmp(data, "w\0") == 0)
             {
                 CAN_Send = 1;
             }
@@ -67,7 +68,7 @@ int main()
             {
                 output[0] = 0;
             }
-        }
+        }//受信データを送信データに整理
         if (CAN_Send == 1)
         {
             output[0] = 8000;
@@ -93,7 +94,7 @@ int main()
         {
             output[1] = 0;
         }
-        CANMessage msg(1, (const uint8_t *)output, 8);
-        can1.write(msg);
+        CANMessage msg(1, (const uint8_t *)output, 8); //メッセージ構築
+        can1.write(msg); //CAN送信
     }
 }
--- src/serials.py
+++ src/serials.py
@@ -5,11 +5,11 @@ import tkinter as tk
 
 class Serials:
 
-    def __init__(self,master,port,baud=115200):
-        self.ser=serial.Serial(port,baud)
+    def __init__(self,master,port,baud=115200): #コンストラクタ
+        self.ser=serial.Serial(port,baud) #シリアル定義
         self.master=master
 
-        send1=tk.Button(master,text="1を送信",command=lambda: self.ser_send("1"))
+        send1=tk.Button(master,text="1を送信",command=lambda: self.ser_send("1"))#ボタン作成
         send2=tk.Button(master,text="2を送信",command=lambda: self.ser_send("2"))
         send3=tk.Button(master,text="3を送信",command=lambda: self.ser_send("3"))
         send1.pack()
@@ -21,26 +21,26 @@ class Serials:
 
         self.keys=[]
 
-        master.bind("<KeyPress>",self.key_press)
+        master.bind("<KeyPress>",self.key_press) #キー入力受け取るための
         master.bind("<KeyRelease>",self.key_release)
 
-    def ser_send(self,send):
+    def ser_send(self,send): #シリアル送信(ボタン用)
         self.ser.write(send.encode())
 
-    def key_press(self,event):
+    def key_press(self,event): #キー入力送信
 
         if event.keysym not in self.keys:
             self.keys.append(event.keysym)
             send=event.keysym+"\0"
             self.ser.write(send.encode())
 
-    def key_release(self,event):
+    def key_release(self,event): #キー解除送信
 
         send=event.keysym+"0\0"
         self.keys.remove(event.keysym)
         self.ser.write(send.encode())
 
-    def ser_read(self):
+    def ser_read(self): #シリアル受信(不使用)
         self.read.config(text=self.ser.readline())
 
 
@@ -50,7 +50,7 @@ class Serials:
 #os.system("xset r off")
 
 root=tk.Tk()
-Serials(root,"COM4")
+Serials(root,"COM3")
 root.mainloop()
 
 #os.system("xset r on")
